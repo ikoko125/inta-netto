dcc44040f89e63a02441d734c1309b6e025cda59
ikoko125 <68625796+ikoko125@users.noreply.github.com>
2021-07-02 11:17:32 +0900

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
-  <Tutorial/>
+  <main>
+    <h1>koke0702_ バーナー</h1>
+  </main>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+html{
+  background-color: #0ff;
+}
+  h1{
+    color: #f00;
+    font-size: 10vw;
+  }  
+</style>
